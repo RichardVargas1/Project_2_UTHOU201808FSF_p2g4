1dd2ca6b472478b0a6c9ffaf275a51ecd4e3acfa
RichardVargas1 <richard.vargas95@gmail.com>
2018-12-03 21:45:44 -0600

    fixed lint errors

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // Require the express npm package
-var express = require("express");
+let express = require("express");
 
 // Setting the port
-var PORT = process.env.PORT || 7500;
+let PORT = process.env.PORT || 7500;
 
-var app = express();
+let app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -14,16 +14,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Require the express-handlebars npm package
-var exphbs = require("express-handlebars");
+let exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes
-var routes = require("");
+let routes = require("");
 
 app.use(routes);
 
 app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+    console.log("App now listening at localhost:" + PORT);
 });
