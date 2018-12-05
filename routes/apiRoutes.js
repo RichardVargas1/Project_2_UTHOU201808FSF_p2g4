21b5821962d0313ea4b9a6176788c3639192f5b1
Your Name <salil_bhise@yahoo.com>
2018-12-05 12:25:20 -0600

    updated login route and boilerplate for models

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+// Register route
 router.post("/register", (req, res) => {
     console.log(req.body);
     res.send(req.body);
 });
-// Create login route like the one above
+
+// Login route
+router.post("/login", (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 module.exports = router;
