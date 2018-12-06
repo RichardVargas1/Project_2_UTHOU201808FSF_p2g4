d53e50130db45afb6b308b592e615c8606c6c2e8
Your Name <salil_bhise@yahoo.com>
2018-12-06 16:14:41 -0600

    prettier and such

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,15 +4,15 @@ const axios = require("axios");
 
 const request = axios.create({
     baseURL: "https://data.energystar.gov/resource",
-    headers: {'X-App-Token': "rSGJanpH6okWzqMSAL4lRi4eh"}
+    headers: { "X-App-Token": "rSGJanpH6okWzqMSAL4lRi4eh" }
 });
 
 const energyStarRoutes = {
-    'fridges': '/ymjh-yrse.json',
-    'dishwashers': '/v32c-ywkg.json',
-    'washers': '/k5sb-ibyp.json',
-    'dryers': '/cv4u-mmnf.json'
-  };
+    fridges: "/ymjh-yrse.json",
+    dishwashers: "/v32c-ywkg.json",
+    washers: "/k5sb-ibyp.json",
+    dryers: "/cv4u-mmnf.json"
+};
 
 // Register route
 router.post("/register", (req, res) => {
@@ -26,10 +26,10 @@ router.post("/login", (req, res) => {
     res.send(req.body);
 });
 
-router.get('/refresh/:cat', async (req, res) => {
+router.get("/refresh/:cat", async (req, res) => {
     const cat = req.params.cat;
     const cats = Object.keys(energyStarRoutes);
-    if(cats.indexOf(cat) === -1) throw new Error('Category does not exist!');
+    if (cats.indexOf(cat) === -1) throw new Error("Category does not exist!");
     const resp = await request.get(energyStarRoutes[cat]);
     console.log(resp.data[0]);
 });
