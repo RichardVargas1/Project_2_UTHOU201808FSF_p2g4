51342ff5a787661c47c861507fc5cf1d46187ef6
Your Name <salil_bhise@yahoo.com>
2018-12-06 21:26:05 -0600

    data visualizations added to public/images underr thier own product folders

---
+++ public/assets/img/dryers/dryerbrands.js
@@ -0,0 +1,114 @@
+
+// GROUPS:  0 Web | 1: Adobe | 2: hybrid
+var data = [
+    {"id": 0, "name": "GE", "r": 31 },
+    {"id": 0, "name": "Inglis", "r": 1 },
+    {"id": 0, "name": "Insignia", "r": 1 },
+    {"id": 0, "name": "Kenmore", "r": 34 },
+    {"id": 0, "name": "Amana", "r": 1 },
+    {"id": 0, "name": "Asko", "r": 1 },
+    {"id": 0, "name": "Beko", "r": 2 },
+    {"id": 0, "name": "Blomberg", "r": 2 },
+    {"id": 0, "name": "Bosch", "r": 3 },
+    {"id": 0, "name": "Crosley", "r": 5 },
+    {"id": 0, "name": "Electrolux", "r": 20 },
+    {"id": 1, "name": "Gaggenau", "r": 1 },
+    {"id": 1, "name": "LG", "r": 54 },
+    {"id": 1, "name": "Maytag", "r": 43 },
+    {"id": 1, "name": "Samsung", "r": 34 },
+    {"id": 1, "name": "Whirlpool", "r": 79 },
+    {"id": 1, "name": "Miele", "r": 3 },
+  ];
+  
+  var width = window.innerWidth,
+      height = 450;
+  
+  var fill = d3.scale.category10();
+  
+  var nodes = [], labels = [],
+      foci = [{x: 0, y: 150}, {x: 350, y: 150}, {x: 200, y: 150}];
+  
+  var svg = d3.select("body").append("svg")
+      .attr("width", "100%")
+      .attr("height", height)
+      //.attr("domflag", '');
+  
+  var force = d3.layout.force()
+      .nodes(nodes)
+      .links([])
+      .charge(-400)
+      //.chargeDistance(200)
+      .gravity(0.1)
+      .friction(0.8)
+      .size([width, height])
+      .on("tick", tick);
+  
+  //var node = svg.selectAll("circle");
+  var node = svg.selectAll("g");
+  
+  var counter = 0;
+  
+  function tick(e) {
+    var k = .1 * e.alpha;
+  
+    // Push nodes toward their designated focus.
+    nodes.forEach(function(o, i) {
+      o.y += (foci[o.id].y - o.y) * k;
+      o.x += (foci[o.id].x - o.x) * k;
+    });
+  
+    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  
+  }
+  
+  
+  var timer = setInterval(function(){
+  
+    if (nodes.length > data.length-1) { clearInterval(timer); return;}
+  
+    var item = data[counter];
+    nodes.push({id: item.id, r: item.r, name: item.name});
+    force.start();
+  
+    node = node.data(nodes);
+  
+    var n = node.enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+        .style('cursor', 'pointer')
+        .on('mousedown', function() {
+           var sel = d3.select(this);
+           sel.moveToFront();
+        })
+        .call(force.drag);
+  
+    n.append("circle")
+        .attr("r",  function(d) { return d.r; })
+        .style("fill", function(d) { return fill(d.id); })
+  
+    n.append("text")
+        .text(function(d){
+            return d.name;
+        })
+        .style("font-size", function(d) {
+            return Math.min(2 * d.r, (2 * d.r - 8) / this.getComputedTextLength() * 16) + "px"; 
+         })
+        .attr("dy", ".35em")
+  
+    counter++;
+  }, 100);
+  
+  
+  d3.selection.prototype.moveToFront = function() {
+    return this.each(function(){
+      this.parentNode.appendChild(this);
+    });
+  };
+  
+  function resize() {
+    width = window.innerWidth;
+    force.size([width, height]);
+    force.start();
+  }
+  
+  d3.select(window).on('resize', resize);
---
+++ public/assets/img/fridges/fridgebrands.js
@@ -0,0 +1,210 @@
+
+// GROUPS:  0 Web | 1: Adobe | 2: hybrid
+var data = [
+    {"id": 0, "name": "Absocold", "r": 8 },
+    {"id": 0, "name": "Amana", "r": 6 },
+    {"id": 0, "name": "Arcitc King", "r": 30 },
+    {"id": 0, "name": "Avalon", "r": 4 },
+    {"id": 0, "name": "Avanti", "r": 54 },
+    {"id": 0, "name": "AVG", "r": 4 },
+    {"id": 0, "name": "Beko", "r": 15 },
+    {"id": 0, "name": "Bertazzoni", "r": 2 },
+    {"id": 0, "name": "Black & Decker", "r": 6 },
+    {"id": 0, "name": "Blomberg", "r": 21 },
+    {"id": 0, "name": "BlueStar", "r": 1 },
+    {"id": 1, "name": "Bosch", "r": 16 },
+    {"id": 1, "name": "Bourdeau Professional Kitchen", "r": 1 },
+    {"id": 1, "name": "Brigade", "r": 3 },
+    {"id": 1, "name": "Capital", "r": 2 },
+    {"id": 1, "name": "Char-Broil", "r": 1 },
+    {"id": 1, "name": "Chefstyle", "r": 1 },
+    {"id": 1, "name": "Classic", "r": 2 },
+    {"id": 1, "name": "Coca-Cola", "r": 2 },
+    {"id": 1, "name": "Commercial Cool", "r": 5 },
+    {"id": 1, "name": "Conserv", "r": 1 },
+    {"id": 1, "name": "Crosley", "r": 30 },
+    {"id": 1, "name": "Cuisinart", "r": 3 },
+    {"id": 1, "name": "Dacor", "r": 11 },
+    {"id": 1, "name": "Daewoo", "r": 15 },
+    {"id": 1, "name": "Danby", "r": 24 },
+    {"id": 1, "name": "Danby Design", "r": 17 },
+      {"id": 1, "name": "Dayton", "r": 7 },
+      {"id": 1, "name": "DCS", "r": 6 },
+      {"id": 1, "name": "Diplomat", "r": 3 },
+      {"id": 1, "name": "EcoMax", "r": 2 },
+      {"id": 1, "name": "Edgestar", "r": 1 },
+    {"id": 1, "name": "Electrolux", "r": 17 },
+    {"id": 1, "name": "ELLIPSE", "r": 9 },
+    {"id": 1, "name": "Emerald Chef", "r": 25 },
+    {"id": 1, "name": "Emerson", "r": 15 },
+    {"id": 1, "name": "EuroDesign", "r": 5 },
+    {"id": 1, "name": "Fagor", "r": 2 },
+    {"id": 1, "name": "Fire Magic", "r": 2 },
+    {"id": 1, "name": "Fisher & Paykel", "r": 11 },
+    {"id": 1, "name": "Frigidaire", "r": 136 },
+    {"id": 1, "name": "FURRION", "r": 4 },
+    {"id": 1, "name": "Gaggenau", "r": 14 },
+    {"id": 1, "name": "Galanz", "r": 21 },
+    {"id": 1, "name": "Garrison", "r": 9 },
+    {"id": 1, "name": "GE", "r": 258 },
+    {"id": 1, "name": "Gladiator", "r": 1 },
+    {"id": 1, "name": "Goldstar", "r": 1 },
+     {"id": 1, "name": "Gorenje", "r": 6 },
+     {"id": 1, "name": "Haier", "r": 9 },
+     {"id": 1, "name": "Hamilton Beach", "r": 14 },
+     {"id": 1, "name": "Hanover", "r": 2 },
+     {"id": 1, "name": "Hisense", "r": 29 },
+    {"id": 1, "name": "Home", "r": 1 },
+    {"id": 1, "name": "Hotpoint", "r": 6 },
+    {"id": 1, "name": "IGLOO", "r": 20 },
+    {"id": 1, "name": "Ikea", "r": 12 },
+    {"id": 1, "name": "Insignia", "r": 27 },
+    {"id": 1, "name": "Jenn-Air", "r": 12 },
+    {"id": 1, "name": "Kalamazoo Outdoor Gourmet", "r": 4 },
+    {"id": 1, "name": "Kegco", "r": 7 },
+    {"id": 1, "name": "Kenmore", "r": 86 },
+    {"id": 1, "name": "Keystone", "r": 6 },
+     {"id": 1, "name": "Kitchen Aid", "r": 61 },
+     {"id": 1, "name": "KOOLATRON", "r": 1 },
+     {"id": 1, "name": "LG", "r": 99 },
+     {"id": 1, "name": "Liebherr", "r": 66 },
+     {"id": 1, "name": "Lorell", "r": 3 },
+     {"id": 1, "name": "Mabe", "r": 3 },
+     {"id": 1, "name": "Magic Chef", "r": 68 },
+     {"id": 1, "name": "Magic Cool", "r": 1 },
+     {"id": 1, "name": "Marathon", "r": 61 },
+     {"id": 1, "name": "Marvel", "r": 53 },
+     {"id": 1, "name": "Maytag", "r": 27 },
+     {"id": 1, "name": "MC Appliance", "r": 1 },
+     {"id": 1, "name": "Meile", "r": 2 },
+    {"id": 1, "name": "MicroChill", "r": 4 },
+    {"id": 1, "name": "MicroFridge", "r": 94 },
+    {"id": 1, "name": "Midea", "r": 57 },
+    {"id": 1, "name": "MIGALI", "r": 2 },
+    {"id": 1, "name": "Moffat", "r": 4 },
+   {"id": 1, "name": "Monogram", "r": 4 },
+    {"id": 1, "name": "Norpole", "r": 1 },
+    {"id": 1, "name": "Panasonic", "r": 2 },
+    {"id": 1, "name": "Perfect aire", "r": 10 },
+    {"id": 1, "name": "Perlick", "r": 9 },
+    {"id": 1, "name": "PREMIUM", "r": 9 },
+    {"id": 1, "name": "RCA", "r": 45 },
+    {"id": 1, "name": "Royal Sovereign", "r": 1 },
+    {"id": 1, "name": "RVISION", "r": 7 },
+    {"id": 1, "name": "Samsung", "r": 69 },
+    {"id": 1, "name": "Season", "r": 8 },
+    {"id": 1, "name": "Signature Kitchen Suite", "r": 7 },
+    {"id": 1, "name": "Silhouette Professional", "r": 8 },
+    {"id": 1, "name": "Smeg", "r": 3 },
+    {"id": 1, "name": "Snackmate", "r": 4 },
+     {"id": 1, "name": "SPT", "r": 10 },
+     {"id": 1, "name": "Sub-Zero", "r": 21 },
+     {"id": 1, "name": "Summit", "r": 37 },
+     {"id": 1, "name": "Sunbeam", "r": 1 },
+     {"id": 1, "name": "Sylvania", "r": 3 },
+    {"id": 1, "name": "Thermador", "r": 3 },
+    {"id": 1, "name": "THORKITCHEN", "r": 3 },
+    {"id": 1, "name": "TRAMONTINA", "r": 3 },
+    {"id": 1, "name": "True Refrigeration", "r": 4 },
+    {"id": 1, "name": "Twin Eagles", "r": 8 },
+    {"id": 1, "name": "U-Line", "r": 24 },
+    {"id": 1, "name": "VIKING", "r": 18 },
+    {"id": 1, "name": "Whirlpool", "r": 76 },
+     {"id": 1, "name": "White-Westinghouse", "r": 3 },
+     {"id": 1, "name": "Whynter", "r": 3 },
+     {"id": 1, "name": "Willz", "r": 1 },
+    
+  ];
+  
+  var width = window.innerWidth,
+      height = 450;
+  
+  var fill = d3.scale.category10();
+  
+  var nodes = [], labels = [],
+      foci = [{x: 0, y: 150}, {x: 350, y: 150}, {x: 200, y: 150}];
+  
+  var svg = d3.select("body").append("svg")
+      .attr("width", "100%")
+      .attr("height", height)
+      //.attr("domflag", '');
+  
+  var force = d3.layout.force()
+      .nodes(nodes)
+      .links([])
+      .charge(-400)
+      //.chargeDistance(200)
+      .gravity(0.1)
+      .friction(0.8)
+      .size([width, height])
+      .on("tick", tick);
+  
+  //var node = svg.selectAll("circle");
+  var node = svg.selectAll("g");
+  
+  var counter = 0;
+  
+  function tick(e) {
+    var k = .1 * e.alpha;
+  
+    // Push nodes toward their designated focus.
+    nodes.forEach(function(o, i) {
+      o.y += (foci[o.id].y - o.y) * k;
+      o.x += (foci[o.id].x - o.x) * k;
+    });
+  
+    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  
+  }
+  
+  
+  var timer = setInterval(function(){
+  
+    if (nodes.length > data.length-1) { clearInterval(timer); return;}
+  
+    var item = data[counter];
+    nodes.push({id: item.id, r: item.r, name: item.name});
+    force.start();
+  
+    node = node.data(nodes);
+  
+    var n = node.enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+        .style('cursor', 'pointer')
+        .on('mousedown', function() {
+           var sel = d3.select(this);
+           sel.moveToFront();
+        })
+        .call(force.drag);
+  
+    n.append("circle")
+        .attr("r",  function(d) { return d.r; })
+        .style("fill", function(d) { return fill(d.id); })
+  
+    n.append("text")
+        .text(function(d){
+            return d.name;
+        })
+        .style("font-size", function(d) {
+            return Math.min(2 * d.r, (2 * d.r - 8) / this.getComputedTextLength() * 16) + "px"; 
+         })
+        .attr("dy", ".35em")
+  
+    counter++;
+  }, 100);
+  
+  
+  d3.selection.prototype.moveToFront = function() {
+    return this.each(function(){
+      this.parentNode.appendChild(this);
+    });
+  };
+  
+  function resize() {
+    width = window.innerWidth;
+    force.size([width, height]);
+    force.start();
+  }
+  
+  d3.select(window).on('resize', resize);
