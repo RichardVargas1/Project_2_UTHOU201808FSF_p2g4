b2bd459296b8f22fc9f4f016b7cdfbc20b16da41
rezwana rubaiya <rezwanarubaiya@gmail.com>
2018-12-06 21:43:28 -0600

    Handlebars changes

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -14,3 +14,14 @@ $("#submit").on("click", function(event) {
 });
 
 // Need to create a event listerner to get login information
+
+// click on button on nav bar
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+   function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
