804d7d640233edb365103c18ce7592489b580e99
RichardVargas1 <richard.vargas95@gmail.com>
2018-12-08 11:04:26 -0600

    commented out example.js

--- models/example.js
+++ models/example.js
@@ -1,8 +1,8 @@
-// Boilerplate code
-module.exports = function(sequelize, DataTypes) {
-    const Example = sequelize.define("Example", {
-        text: DataTypes.STRING,
-        description: DataTypes.TEXT
-    });
-    return Example;
-};
+// // Boilerplate code
+// module.exports = function(sequelize, DataTypes) {
+//     const Example = sequelize.define("Example", {
+//         text: DataTypes.STRING,
+//         description: DataTypes.TEXT
+//     });
+//     return Example;
+// };
