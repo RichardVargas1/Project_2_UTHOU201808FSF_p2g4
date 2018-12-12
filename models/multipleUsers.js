35b5c9355a5ff51c94fa90e9d5a7925f24e3787c
RichardVargas1 <richard.vargas95@gmail.com>
2018-12-11 18:05:01 -0600

    Updates

--- models/multipleUsers.js
+++ models/multipleUsers.js
@@ -1,37 +1,15 @@
-// helps navigate multiple user sign-ups
-
 module.exports = function (sequelize, DataTypes) {
 	const multipleUsers = sequelize.define("multipleUsers", {
-		username: {
-			type: DataTypes.STRING,
-      		allowNull: false,
-					unique: true,
-      		validate: {
-        		len: [1]
-      		}
-		},
-		password: {
-			type: DataTypes.STRING.BINARY,
-      		allowNull: false,
-      		validate: {
-        		len: [1]
-      		}
-		},
-		email: {
-			type: DataTypes.STRING,
-      		allowNull: false,
-					unique: true,
-      		validate: {
-        		len: [1]
-      		}
-		}
+		// Giving the users a type name STRING
+		name: DataTypes.STRING
 	});
 
-	multipleUsers.associate = function(models) {
-		// Associating User Profiles, with Posts. When an User is deleted, this will also delete any associated Posts
-		multipleUsers.hasMany(models.history, {
-		  onDelete: "cascade"
+	multipleUsers.associate = function (models) {
+		// Associating users with Posts
+		Author.hasMany(models.Post, {
+			onDelete: "cascade"
 		});
 	};
+
 	return multipleUsers;
-}
+};
